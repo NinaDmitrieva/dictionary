350ba5d3c5f2d75a102f2cadb91abca72fe9f9ed
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2023-02-14 13:20:51 +0300

    слово в контейнерe results-word

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,7 @@
-
+// получить данные по апи
+//вставить слово в контейнер results-word
+//добавить функционал для воспроизведения звука
+//вставить полученные данные в контейнер с результатами
 
 let state = {
     word: ""
@@ -6,6 +9,14 @@ let state = {
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 const input = document.getElementById('word-input');
 const form = document.querySelector('.form');
+const containerWord = document.querySelector('.results-word');
+
+
+const insertWord = () => {
+    containerWord.innerText = state.word;
+    console.log('hi')
+    
+}
 
 const hendleSubmit = async (e) => {
     e.preventDefault();
@@ -13,18 +24,18 @@ const hendleSubmit = async (e) => {
     if (!state.word.trim()) return;
 
     try {
-        const response = await fetch(`${url}${state.value}`);
+        const response = await fetch(`${url}${state.word}`);
         const data = await response.json();
 
         if(response.ok && data.length) {
-
+            insertWord();
         }
     } catch (err){
         console.log(err);
     }
 
-    
 };
+
 
 const hendleKeyup = (e) => {
     const value = e.target.value;
