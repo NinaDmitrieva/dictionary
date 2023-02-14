06b5fc8b720c9d73af3eeb23a3d3e99f5e5f4ca0
Nina Dmitrieva <nina.dmitrieva.spb@yandex.ru>
2023-02-14 13:46:10 +0300

    воспроизведение икомого слова

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,18 +4,23 @@
 //вставить полученные данные в контейнер с результатами
 
 let state = {
-    word: ""
+    word: "",
+    meanings: [],
+    phonetics: []
+
 };
 const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 const input = document.getElementById('word-input');
 const form = document.querySelector('.form');
 const containerWord = document.querySelector('.results-word');
+const soundBtn = document.querySelector('.results-sound');
+
 
 
 const insertWord = () => {
     containerWord.innerText = state.word;
     console.log('hi')
-    
+
 }
 
 const hendleSubmit = async (e) => {
@@ -27,10 +32,16 @@ const hendleSubmit = async (e) => {
         const response = await fetch(`${url}${state.word}`);
         const data = await response.json();
 
-        if(response.ok && data.length) {
+        if (response.ok && data.length) {
+            const item = data[0];
+            state = {
+                ...state,
+                meanings: item.meanings,
+                phonetics: item.phonetics,
+            }
             insertWord();
         }
-    } catch (err){
+    } catch (err) {
         console.log(err);
     }
 
@@ -42,6 +53,17 @@ const hendleKeyup = (e) => {
     state.word = value;
 };
 
+const heandleSound = () => {
+    if (state.phonetics.length) {
+        const sound = state.phonetics[0];
+
+        if(sound.audio) {
+            new Audio(sound.audio).play();
+        }
+    }
+}
+
 //event
 input.addEventListener('keyup', hendleKeyup);
 form.addEventListener('submit', hendleSubmit);
+soundBtn.addEventListener('click', heandleSound)
